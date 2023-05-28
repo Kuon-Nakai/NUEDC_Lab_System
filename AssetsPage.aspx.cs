5f361e0fc11fd177e395c6295a157b263f19b3d6
Kuon-Nakai <110766343+Kuon-Nakai@users.noreply.github.com>
2023-05-28 15:45:43 +0800

    Bugfixes for assets lending

--- AssetsPage.aspx.cs
+++ AssetsPage.aspx.cs
@@ -2,15 +2,19 @@
 
 using MySql.Data.MySqlClient;
 using System;
+using System.CodeDom;
+using System.Collections.Generic;
+using System.Drawing;
 using System.Threading;
 using System.Threading.Tasks;
+using System.Web.UI.WebControls;
 
 public partial class AssetsPage : System.Web.UI.Page
 {
     private DynamicControls dc = new DynamicControls();
     MySqlSvr svr;
     bool loggedIn = false;
-    string userId;
+    public string userId;
 
     protected async void Page_Load(object sender, EventArgs e)
     {
@@ -19,6 +23,8 @@ public partial class AssetsPage : System.Web.UI.Page
         {
             loggedIn = true;
             userId = (string)Session["UserID"];
+            Borrow_tb.OnClientClick = "showBorrowConfirmPopup();";
+            Borrow_tb.Attributes["href"] = "#";
         }
         if (IsPostBack) { return; }
         svr = new MySqlSvr(new MySqlConnection("server=127.0.0.1; database=nuedc; user id=notRoot; password=1234"));
@@ -44,7 +50,7 @@ public partial class AssetsPage : System.Web.UI.Page
         InitiateSearch("select AssetCode, AssetName, MainValue, ValueUnit from assets left join assetclasses on assets.ClassCode = assetclasses.ClassCode;");
         dc.CreateAlert("This is a debug version of the website. Content may change at any time.", "notice", Alerts_pn);
         //DynamicControls.CreateAlert("Info", "info", Alerts_pn);
-        LoadAssetData($"assets.AssetCode='{Asset_gv.Rows[0].Cells[1].Text}'");
+        LoadAssetData($"assets.AssetCode='{Asset_gv.Rows[0].Cells[0].Text}'");
     }
 
     private void InitiateSearch(string sql)
@@ -91,7 +97,7 @@ public partial class AssetsPage : System.Web.UI.Page
     {
         try
         {
-            return ((int?)Session["UserPerm"] ?? 256) <= (int)svr.QuerySingle($"select LendingPolicy from assets where AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[1].Text}';");
+            return ((int?)Session["UserPerm"] ?? 256) <= (svr.QuerySingle($"select LendingPolicy from assets where AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[0].Text}';") as int?);
         }
         catch (Exception)
         {
@@ -107,26 +113,30 @@ public partial class AssetsPage : System.Web.UI.Page
             // All web side checks pass
             try
             {
-                if (svr.QuerySingle($"select TransactionCode from lending where AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[1].Text}' and FullCode='{ItemID_tb.Text}' and not TransactionCycleEnded;").ToString().Length > 0)
+                if (svr == null) svr = new MySqlSvr("server=127.0.0.1; database=nuedc; user id=notRoot; password=1234");
+                if (svr.QuerySingle($"select TransactionCode from lending where AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[0].Text}' and FullCode='{ItemID_tb.Text}' and not TransactionCycleEnded;") != null)
                 {
                     dc.CreateAlert("数据不一致: 该元件已经处于借出状态", "error", Alerts_pn);
                     return;
                 }
                 // Data consistency check pass
-                if ((DateTime)Application["LastReqDate"] != DateTime.Today)
+                if (Application["LastReqDate"] == null || (DateTime)Application["LastReqDate"] != DateTime.Today)
                 {
                     Application["LastReqDate"] = DateTime.Today;
                     Application["TransactionCount"] = 0;
                 }
+                svr.cn.Open();
                 using (var transact = svr.cn.BeginTransaction())
                 {
+                    var permitted = false;
+                    // Synchronization lock to prevent multiple requests conflicting
                     lock (this)
                     {
                         if (svr.Execute("insert into lending values(" +
-                            $"'{DateTime.Today.ToString("yyyy-MM-dd").Replace("-", "") + string.Format("%4d", (int)Application["TransactionCount"])}'," + // TransactionCode
-                            $"'{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[1].Text}'," + // AssetCode
+                            $"'{DateTime.Today.ToString("yyyy-MM-dd").Replace("-", "") + ((int)Application["TransactionCount"]).ToString("0000")}'," + // TransactionCode
+                            $"'{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[0].Text}'," + // AssetCode
                             $"{(BorrowQtySel_tb.Enabled ? int.Parse(BorrowQtySel_tb.Text) : 1)}," + // Qty
-                            $"{(ItemID_tb.Enabled ? ItemID_tb.Text : "null")}," + // FullCode
+                            $"{(ItemID_tb.Enabled ? (Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[0].Text + ItemID_tb.Text : "null")}," + // FullCode
                             $"{userId}," + // MemberCode
                             "'Requested'," + // Status
                             "NOW()," + // DateRequested
@@ -138,45 +148,64 @@ public partial class AssetsPage : System.Web.UI.Page
                         {
                             dc.CreateAlert("数据库操作异常/数据插入", "error", Alerts_pn);
                             transact.Rollback();
+                            svr.cn.Close();
                             return;
                         }
-                        if (Assert_BorrowPerm())
+                        var temp = (int)Application["TransactionCount"];
+                        Application["TransactionCount"] = temp + 1;
+                        //transact.Commit();
+                        if (!Assert_BorrowPerm())
                         {
                             // Permission level reached, system will auto approve the request
                             if (svr.Execute("update lending set " +
-                                $"DateProcessed=NOW()," +
-                                $"Status='Taken'," +
-                                $"Note='Automatically approved by system'" +
-                                $"where TransactionCode={DateTime.Today.ToString("yyyy-MM-dd").Replace("-", "") + string.Format("%4d", (int)Application["TransactionCount"])};",
+                                "DateProcessed=NOW()," +
+                                "Status='Taken'," +
+                                "Note='Automatically approved by system'" +
+                                $"where TransactionCode={DateTime.Today.ToString("yyyy-MM-dd").Replace("-", "") + ((int)Application["TransactionCount"] - 1).ToString("0000")};",
                                 transact) != 1)
                             {
                                 dc.CreateAlert("数据库操作异常/数据修改-1", "error", Alerts_pn);
                                 transact.Rollback();
+                                svr.cn.Close();
                                 return;
                             }
+                            else
+                            {
+                                permitted = true;
+                            }
                         }
-                        if ((int)svr.QuerySingle($"select AutoCplt from assetclasses right join assets on assetclasses.ClassCode=assets.ClassCode where assets.AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[1].Text}") == 1)
+                        //transact.Commit();
+                        if ((ulong)svr.QuerySingle($"select AutoCplt from assetclasses right join assets on assetclasses.ClassCode=assets.ClassCode where assets.AssetCode='{(Asset_gv.SelectedRow ?? Asset_gv.Rows[0]).Cells[0].Text}';") == 1)
                         {
                             // Transaction cycle will be marked as complete
                             if (svr.Execute("update lending set " +
-                                "TransactionCycleEnded = 1",
+                                "TransactionCycleEnded = 1" +
+                                $"where TransactionCode={DateTime.Today.ToString("yyyy-MM-dd").Replace("-", "") + ((int)Application["TransactionCount"] - 1).ToString("0000")};",
                                 transact) != 1)
                             {
                                 dc.CreateAlert("数据库操作异常/数据修改-2", "error", Alerts_pn);
                                 transact.Rollback();
+                                svr.cn.Close();
                                 return;
                             }
                         }
-                        var temp = (int)Application["TransactionCount"];
-                        Application["TransactionCount"] = temp + 1;
                     }
                     transact.Commit();
+                    svr.cn.Close();
+                    Borrow_tb.Text = "登记成功!";
+                    Borrow_tb.CssClass = "btn btn--primary u-fullwidth";
+                    Borrow_tb.Enabled = true;
+                    dc.CreateAlert("登记成功, 审核通过后即可取走元件!", "success", Alerts_pn);
+                    if (permitted)
+                        dc.CreateAlert("审核已通过, 请自行取走元件!", "success", Alerts_pn);
                 }
 
             }
             catch (Exception)
             {
+                //throw e;
                 dc.CreateAlert("数据库操作错误", "error", Alerts_pn);
+                svr.cn.CloseAsync();
             }
         }
     }
@@ -185,7 +214,7 @@ public partial class AssetsPage : System.Web.UI.Page
     {
         if (svr == null) { svr = new MySqlSvr("server=127.0.0.1; database=nuedc; user id=notRoot; password=1234"); }
         int i = -1;
-        svr.QueryReader($"select AssetName, ClassName, MainValue, ValueUnit, Location, Characteristics, Amount-ReservationQty-(select count(TransactionCode), AutoCplt from lending right join assets on lending.AssetCode=assets.AssetCode where {sql_where_assets} and Status='Returned') from assets left join assetclasses on assets.ClassCode=assetclasses.ClassCode where {sql_where_assets};",
+        svr.QueryReader($"select AssetName, ClassName, MainValue, ValueUnit, Location, Characteristics, Amount-ReservationQty-(select count(TransactionCode) from lending right join assets on lending.AssetCode=assets.AssetCode where {sql_where_assets} and Status='Returned'), AutoCplt from assets left join assetclasses on assets.ClassCode=assetclasses.ClassCode where {sql_where_assets};",
             AssignAssetData,
             /*() => Alert_DBQueryEmpty_pn.Visible = true*/
             () => dc.CreateAlert("元件信息查询失败: 返回信息为空", "error", Alerts_pn));
@@ -212,7 +241,7 @@ public partial class AssetsPage : System.Web.UI.Page
         PrimValue_lb.Text = $"{rd[2]} {rd[3]}";
         Location_lb.Text = (string)rd[4];
         Property_lb.Text = rd[5].ToString().Length > 0 ? (string)rd[5] : "暂无数据";
-        if ((bool)rd[6])
+        if ((ulong)rd[7] == 1)
         {
             BorrowQtySel_tb.Enabled = true;
             ItemID_tb.Enabled = false;
@@ -254,7 +283,7 @@ public partial class AssetsPage : System.Web.UI.Page
     {
         string sear_str = sear_tb.Text;
         InitiateSearch($"select AssetCode as 元件代码, AssetName as 元件名称, MainValue as 值, ValueUnit as 单位 from assets left join assetclasses on assets.ClassCode = assetclasses.ClassCode where AssetCode like '%{sear_str}%' or AssetName like '%{sear_str}%';");
-        LoadAssetData($"assets.AssetCode='{Asset_gv.Rows[0].Cells[1].Text}'");
+        LoadAssetData($"assets.AssetCode='{Asset_gv.Rows[0].Cells[0].Text}'");
     }
 
     protected void AwaitReturn_bt_Click(object sender, EventArgs e)
@@ -286,6 +315,28 @@ public partial class AssetsPage : System.Web.UI.Page
 
     protected void Asset_gv_SelectedIndexChanged(object sender, EventArgs e)
     {
-        LoadAssetData($"assets.AssetCode='{Asset_gv.SelectedRow.Cells[1].Text}'");
+        LoadAssetData($"assets.AssetCode='{Asset_gv.SelectedRow.Cells[0].Text}'");
+    }
+
+    protected void Asset_gv_RowCreated(object sender, GridViewRowEventArgs e)
+    {
+        if (e.Row.RowType == DataControlRowType.DataRow)
+        {
+            e.Row.Attributes["onmouseover"] = "this.style.cursor='pointer';this.style.background-color='#ffe5cc';";
+            e.Row.Attributes["onmouseout"] = "this.style.background-color='none';";
+            e.Row.ToolTip = "点击选择";
+            e.Row.Attributes["onclick"] = Page.ClientScript.GetPostBackClientHyperlink(Asset_gv, "Select$" + e.Row.RowIndex);
+            
+        }
+    }
+
+    protected void Login_Jmp_bt_Click(object sender, EventArgs e)
+    {
+        if (Session["UserID"] == null)
+        {
+            if (Session["jmpStack"] == null) Session["jmpStack"] = new Stack<string>();
+            ((Stack<string>)Session["jmpStack"]).Push(Request.RawUrl);
+            Response.Redirect("Login_Reg.aspx");
+        }
     }
 }
